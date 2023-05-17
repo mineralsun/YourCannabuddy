e7057c776cf6b10412c2a001af1242c12ea41c61
Everett Butler <everettjamesbutler@gmail.com>
2023-05-16 19:38:00 -0500

    SQL query in database.sql is set up and tables are made!

--- database.sql
+++ database.sql
@@ -1,10 +1,27 @@
-
--- USER is a reserved keyword with Postgres
--- You must use double quotes in every query that user is in:
--- ex. SELECT * FROM "user";
--- Otherwise you will have errors!
 CREATE TABLE "user" (
     "id" SERIAL PRIMARY KEY,
     "username" VARCHAR (80) UNIQUE NOT NULL,
     "password" VARCHAR (1000) NOT NULL
+);
+
+CREATE TABLE "productType" (
+	"id" SERIAL PRIMARY KEY,
+	"typeName" VARCHAR(100)
+);
+
+CREATE TABLE "topEffect" (
+	"id" SERIAL PRIMARY KEY,
+	"top_effect_name" VARCHAR(100)
+);
+
+CREATE TABLE "products" (
+	"id" SERIAL PRIMARY KEY,
+	"user_id" INT,
+	"product_name" VARCHAR(1000) NOT NULL,
+	"brand_name" VARCHAR(100) NOT NULL,
+	"product_id" INT REFERENCES "productType",
+	"rating" INT DEFAULT(0) NOT NULL,
+	"comments" VARCHAR(50000),
+	"top_effect_id" INT REFERENCES "topEffect",
+	"isFavorite" BOOLEAN DEFAULT(FALSE) NOT NULL
 );
