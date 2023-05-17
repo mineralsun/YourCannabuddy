36ede76cb94def6a83ef05002a47ff701d0eb9c9
Everett Butler <everettjamesbutler@gmail.com>
2023-05-16 19:55:38 -0500

    got test data to work and now in the database.sql for new users to test their database

--- database.sql
+++ database.sql
@@ -16,7 +16,7 @@ CREATE TABLE "topEffect" (
 
 CREATE TABLE "products" (
 	"id" SERIAL PRIMARY KEY,
-	"user_id" INT,
+	"user_id" INT REFERENCES "user",
 	"product_name" VARCHAR(1000) NOT NULL,
 	"brand_name" VARCHAR(100) NOT NULL,
 	"product_id" INT REFERENCES "productType",
@@ -25,3 +25,17 @@ CREATE TABLE "products" (
 	"top_effect_id" INT REFERENCES "topEffect",
 	"isFavorite" BOOLEAN DEFAULT(FALSE) NOT NULL
 );
+
+INSERT INTO "user" ("username", "password")
+VALUES ('your_name_here', 'your_password_here');
+
+INSERT INTO "productType" ("typeName")
+VALUES ('Flower'), ('Pre-Rolls'), ('Edibles'), ('Vaporizers'), ('Concentrates'), ('Topicals'), ('Tinctures');
+
+INSERT INTO "topEffect" ("top_effect_name")
+VALUES ('Calm'), ('Pain-Relief'), ('Clear-Mind'), ('Creative'), ('Energetic'), ('Focused'), ('Happy'), ('Inspired'), ('Relaxed'), ('Sleepy');
+
+
+
+INSERT INTO "products" ("user_id", "product_name", "brand_name", "product_id", "rating", "comments", "top_effect_id", "isFavorite")
+VALUES (1, 'Ghost OG', 'Illicit', 1, 5, 'One of the best hybrids on the market. Energetic in the head, relaxing in the body. Truly the best of both worlds!', 5, TRUE);
