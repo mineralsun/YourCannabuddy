6723d6fbb6973d9590ff1392c10807196274acac
Everett Butler <everettjamesbutler@gmail.com>
2023-05-18 16:13:22 -0500

    began working on form for new product

--- src/components/Stash/stash.jsx
+++ src/components/Stash/stash.jsx
@@ -5,7 +5,7 @@ import axios from 'axios';
 function Stash() {
     const dispatch = useDispatch();
     const products = useSelector(store => store.productList)
-    const [newProduct, setNewProduct] = useState({ 
+    const [newProduct, setNewProduct] = useState({
         productName: '',
         brandName: '',
         product_id: 0,
@@ -21,16 +21,16 @@ function Stash() {
 
     const handleChangeFor = (key, value) => {
         console.log('event happened');
-        setNewProduct({...newProduct, [key]: value});
+        setNewProduct({ ...newProduct, [key]: value });
     }
 
     const addProduct = (event) => {
         event.preventDefault();
         dispatch({ type: 'ADD_PRODUCT', payload: newProduct });
-        setNewProduct({ 
-            id: newProduct.id +1, 
-            productName: '',
-            brandName: '',
+        setNewProduct({
+            id: newProduct.id + 1,
+            product_name: '',
+            brand_name: '',
             product_id: 0,
             rating: 0,
             comments: '',
@@ -63,12 +63,58 @@ function Stash() {
             <form onSubmit={addProduct}>
                 <input
                     type='text'
+                    value={newProduct.product_name}
+                    placeholder='Product Name'
+                    onChange={(event) => handleChangeFor('product_name', event.target.value)}
+                />
+                <br />
+                <input
+                    type='text'
+                    value={newProduct.brand_name}
+                    placeholder='Brand Name'
+                    onChange={(event) => handleChangeFor('brand_name', event.target.value)}
+                />
+                <br />
+                <label for="productType">Product Type:</label> <span />
+                <select name="dog-names" id="dog-names">
+                    <option value="Flower">Flower</option>
+                    <option value="Pre-Rolls">Pre-Rolls</option>
+                    <option value="Edibles">Edibles</option>
+                    <option value="Vaporizers">Vaporizers</option>
+                    <option value="Concentrates">Concentrates</option>
+                    <option value="Topicals">Topicals</option>
+                    <option value="Tinctures">Tinctures</option>
+                </select>
+                <br />
+                <input
+                    type='text'
+                    value={newProduct.productName}
+                    placeholder='Product Name'
+                    onChange={(event) => handleChangeFor('product_name', event.target.value)}
+                />
+                <br />
+                <input
+                    type='text'
+                    value={newProduct.productName}
+                    placeholder='Product Name'
+                    onChange={(event) => handleChangeFor('product_name', event.target.value)}
+                />
+                <br />
+                <input
+                    type='text'
+                    value={newProduct.productName}
+                    placeholder='Product Name'
+                    onChange={(event) => handleChangeFor('product_name', event.target.value)}
+                />
+                <br />
+                <input
+                    type='text'
                     value={newProduct.productName}
                     placeholder='Product Name'
                     onChange={(event) => handleChangeFor('product_name', event.target.value)}
                 />
             </form>
-        
+
         </div>
     )
 }
