22aa83816d65418cd5cdc7cc4b9d85a56ba1fc40
Everett Butler <everettjamesbutler@gmail.com>
2023-05-17 19:05:05 -0500

    created products reducer

---
+++ src/redux/reducers/products.reducer.js
@@ -0,0 +1,10 @@
+const productList = (state = [], action) => {
+    switch (action.type) {
+        case 'SET_PRODUCTS':
+            return action.payload;
+        default:
+            return state;
+    }
+};
+
+export default productList;
